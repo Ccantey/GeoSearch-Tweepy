ab68ec7d84253b9c6d3f29859ab60ac4d215f057
Chris Cantey <cantey0701@gmail.com>
2017-12-14 11:01:37 -0600

    The returned twitter status object

---
+++ example/twitterObject.py
@@ -0,0 +1,378 @@
+#Here is an example of the python object the Twitter API returns
+Status(
+	quote_count=0, 
+	contributors=None, 
+	truncated=True, 
+	text=u'@realDonaldTrump You are the absolute definition of stupid.  Or #MORON as your staff would say. #MAGA is\u2026 https://t.co/YT2ziLuWM8', 
+	is_quote_status=False, 
+	in_reply_to_status_id=940584383605563392L, 
+	reply_count=0, 
+	id=940625357748633600L, 
+	favorite_count=0, 
+	_api=<tweepy.api.API object at 0x06AD9050>, 
+	source=u'Twitter for Android', 
+	_json={
+		u'quote_count': 0, 
+		u'contributors': None, 
+		u'truncated': True, 
+		u'text': u'@realDonaldTrump You are the absolute definition of stupid.  Or #MORON as your staff would say. #MAGA is\u2026 https://t.co/YT2ziLuWM8', 
+		u'is_quote_status': False, 
+		u'in_reply_to_status_id': 940584383605563392L, 
+		u'reply_count': 0, 
+		u'id': 940625357748633600L, 
+		u'favorite_count': 0, 
+		u'entities': {
+			u'user_mentions': [
+				{
+					u'id': 25073877, 
+					u'indices': [0, 16], 
+					u'id_str': u'25073877', 
+					u'screen_name': u'realDonaldTrump', 
+					u'name': u'Donald J. Trump'
+				}
+			], 
+			u'symbols': [], 
+			u'hashtags': [
+				{
+					u'indices': [64, 70], 
+					u'text': u'MORON'
+				}, 
+				{
+					u'indices': [96, 101], 
+					u'text': u'MAGA'
+				}
+			], 
+			u'urls': [
+				{
+					u'url': u'https://t.co/YT2ziLuWM8', 
+					u'indices': [106, 129], 
+					u'expanded_url': u'https://twitter.com/i/web/status/940625357748633600', 
+					u'display_url': u'twitter.com/i/web/status/9\u2026'
+				}
+			]
+		}, 
+		u'retweeted': False, 
+		u'coordinates': None, 
+		u'timestamp_ms': u'1513097535918', 
+		u'source': u'<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>', 
+		u'in_reply_to_screen_name': u'realDonaldTrump', 
+		u'id_str': u'940625357748633600', 
+		u'display_text_range': [17, 140], 
+		u'retweet_count': 0, 
+		u'in_reply_to_user_id': 25073877, 
+		u'favorited': False, 
+		u'user': {
+			u'follow_request_sent': None, 
+			u'profile_use_background_image': True, 
+			u'default_profile_image': False, 
+			u'id': 358435647, 
+			u'default_profile': True, 
+			u'verified': False, 
+			u'profile_image_url_https': u'https://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+			u'profile_sidebar_fill_color': u'DDEEF6', 
+			u'profile_text_color': u'333333', 
+			u'followers_count': 1302, 
+			u'profile_sidebar_border_color': u'C0DEED', 
+			u'id_str': u'358435647', 
+			u'profile_background_color': u'C0DEED', 
+			u'listed_count': 42, 
+			u'profile_background_image_url_https': u'https://abs.twimg.com/images/themes/theme1/bg.png', 
+			u'utc_offset': None, 
+			u'statuses_count': 42976, 
+			u'description': u"Twitter allows me to yell, hey you kids, get off my lawn. So the universe can hear. Follow me, I'll appreciate it. Not real fond of trump. #BeGoneTrump.", 
+			u'friends_count': 1657, 
+			u'location': None, 
+			u'profile_link_color': u'1DA1F2', 
+			u'profile_image_url': u'http://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+			u'following': None, 
+			u'geo_enabled': True, 
+			u'profile_background_image_url': u'http://abs.twimg.com/images/themes/theme1/bg.png', 
+			u'name': u'Paul T Murphy', 
+			u'lang': u'en', 
+			u'profile_background_tile': False, 
+			u'favourites_count': 10718, 
+			u'screen_name': u'Ptmurf1016', 
+			u'notifications': None, 
+			u'url': None, 
+			u'created_at': u'Fri Aug 19 22:45:37 +0000 2011', 
+			u'contributors_enabled': False, 
+			u'time_zone': None, 
+			u'protected': False, 
+			u'translator_type': u'none', 
+			u'is_translator': False
+		}, 
+		u'geo': None, 
+		u'in_reply_to_user_id_str': u'25073877', 
+		u'lang': u'en', 
+		u'extended_tweet': {
+			u'display_text_range': [17, 142], 
+			u'entities': {
+				u'user_mentions': [
+					{
+						u'id': 25073877, 
+						u'indices': [0, 16], 
+						u'id_str': u'25073877', 
+						u'screen_name': u'realDonaldTrump', 
+						u'name': u'Donald J. Trump'
+					}
+				], 
+				u'symbols': [], 
+				u'hashtags': [
+					{
+						u'indices': [64, 70], 
+						u'text': u'MORON'
+					}, 
+					{
+						u'indices': [96, 101], 
+						u'text': u'MAGA'
+					}, 
+					{
+						u'indices': [105, 126], 
+						u'text': u'MuellerAintGoingAway'
+					}], 
+				u'urls': []
+			}, 
+			u'full_text': u"@realDonaldTrump You are the absolute definition of stupid.  Or #MORON as your staff would say. #MAGA is #MuellerAintGoingAway.  You're toast."
+		}, 
+		u'created_at': u'Tue Dec 12 16:52:15 +0000 2017', 
+		u'filter_level': u'low', 
+		u'in_reply_to_status_id_str': 
+		u'940584383605563392', 
+		u'place': None
+	},
+	coordinates=None, 
+	timestamp_ms=u'1513097535918', 
+	entities={
+		u'user_mentions': [
+			{
+				u'id': 25073877, 
+				u'indices': [0, 16], 
+				u'id_str': u'25073877', 
+				u'screen_name': u'realDonaldTrump', 
+				u'name': u'Donald J. Trump'
+			}
+		], 
+		u'symbols': [], 
+		u'hashtags': [
+			{
+				u'indices': [64, 70], 
+				u'text': u'MORON'
+			}, {
+				u'indices': [96, 101], 
+				u'text': u'MAGA'
+			}], 
+		u'urls': [
+			{
+				u'url': u'https://t.co/YT2ziLuWM8', 
+				u'indices': [106, 129], 
+				u'expanded_url': u'https://twitter.com/i/web/status/940625357748633600', 
+				u'display_url': 
+				u'twitter.com/i/web/status/9\u2026'
+			}
+		]
+	}, 
+	in_reply_to_screen_name=u'realDonaldTrump', 
+	in_reply_to_user_id=25073877, 
+	display_text_range=[17, 140], 
+	retweet_count=0, 
+	id_str=u'940625357748633600', 
+	favorited=False, 
+	source_url=u'http://twitter.com/download/android', 
+	user=User(
+		follow_request_sent=None, 
+		profile_use_background_image=True,
+		_json={
+			u'follow_request_sent': None, 
+			u'profile_use_background_image': True, 
+			u'default_profile_image': False, 
+			u'id': 358435647, 
+			u'default_profile': True, 
+			u'verified': False, 
+			u'profile_image_url_https': 
+			u'https://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+			u'profile_sidebar_fill_color': u'DDEEF6', 
+			u'profile_text_color': u'333333', 
+			u'followers_count': 1302, 
+			u'profile_sidebar_border_color': u'C0DEED', 
+			u'id_str': u'358435647', 
+			u'profile_background_color': u'C0DEED', 
+			u'listed_count': 42, 
+			u'profile_background_image_url_https': u'https://abs.twimg.com/images/themes/theme1/bg.png', 
+			u'utc_offset': None, 
+			u'statuses_count': 42976, 
+			u'description': u"Twitter allows me to yell, hey you kids, get off my lawn. So the universe can hear. Follow me, I'll appreciate it. Not real fond of trump. #BeGoneTrump.", 
+			u'friends_count': 1657, 
+			u'location': None, 
+			u'profile_link_color': u'1DA1F2', 
+			u'profile_image_url': u'http://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+			u'following': None, 
+			u'geo_enabled': True, 
+			u'profile_background_image_url': u'http://abs.twimg.com/images/themes/theme1/bg.png', 
+			u'name': u'Paul T Murphy', 
+			u'lang': u'en', 
+			u'profile_background_tile': False, 
+			u'favourites_count': 10718, 
+			u'screen_name': u'Ptmurf1016', 
+			u'notifications': None, 
+			u'url': None, 
+			u'created_at': u'Fri Aug 19 22:45:37 +0000 2011', 
+			u'contributors_enabled': False, 
+			u'time_zone': None, 
+			u'protected': False, 
+			u'translator_type': u'none', 
+			u'is_translator': False
+		}, 
+		id=358435647, 
+		_api=<tweepy.api.API object at 0x06AD9050>, 
+		verified=False, 
+		translator_type=u'none', 
+		profile_image_url_https=u'https://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+		profile_sidebar_fill_color=u'DDEEF6', 
+		is_translator=False, 
+		geo_enabled=True, 
+		profile_text_color=u'333333', 
+		followers_count=1302, 
+		protected=False, 
+		location=None, 
+		default_profile_image=False, 
+		id_str=u'358435647', 
+		utc_offset=None, 
+		statuses_count=42976, 
+		description=u"Twitter allows me to yell, hey you kids, get off my lawn. So the universe can hear. Follow me, I'll appreciate it. Not real fond of trump. #BeGoneTrump.", 
+		friends_count=1657, 
+		profile_link_color=u'1DA1F2',
+		profile_image_url=u'http://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+		notifications=None, 
+		profile_background_image_url_https=u'https://abs.twimg.com/images/themes/theme1/bg.png', 
+		profile_background_color=u'C0DEED', 
+		profile_background_image_url=u'http://abs.twimg.com/images/themes/theme1/bg.png', 
+		screen_name=u'Ptmurf1016', 
+		lang=u'en', 
+		profile_background_tile=False, 
+		favourites_count=10718, 
+		name=u'Paul T Murphy', 
+		url=None, 
+		created_at=datetime.datetime(2011, 8, 19, 22, 45, 37), 
+		contributors_enabled=False, 
+		time_zone=None, 
+		profile_sidebar_border_color=u'C0DEED', 
+		default_profile=True, 
+		following=False, 
+		listed_count=42), 
+	geo=None, 
+	in_reply_to_user_id_str=u'25073877', 
+	lang=u'en', 
+	extended_tweet={
+		u'display_text_range': [17, 142], 
+		u'entities': {
+			u'user_mentions': [
+				{
+					u'id': 25073877, 
+					u'indices': [0, 16], 
+					u'id_str': u'25073877', 
+					u'screen_name': u'realDonaldTrump', 
+					u'name': u'Donald J. Trump'
+				}], 
+			u'symbols': [], 
+			u'hashtags': [
+				{
+					u'indices': [64, 70], 
+					u'text': u'MORON'
+				}, 
+				{
+					u'indices': [96, 101], 
+					u'text': u'MAGA'
+				}, 
+				{
+					u'indices': [105, 126], 
+					u'text': u'MuellerAintGoingAway'
+				}
+			], 
+			u'urls': []
+		}, 
+		u'full_text': u"@realDonaldTrump You are the absolute definition of stupid.  Or #MORON as your staff would say. #MAGA is #MuellerAintGoingAway.  You're toast."
+	}, 
+	created_at=datetime.datetime(2017, 12, 12, 16, 52, 15), 
+	author=User(
+		follow_request_sent=None, 
+		profile_use_background_image=True, 
+		_json={
+			u'follow_request_sent': None, 
+			u'profile_use_background_image': True, 
+			u'default_profile_image': False, 
+			u'id': 358435647, 
+			u'default_profile': True, 
+			u'verified': False, 
+			u'profile_image_url_https': u'https://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+			u'profile_sidebar_fill_color': u'DDEEF6', 
+			u'profile_text_color': u'333333', 
+			u'followers_count': 1302, 
+			u'profile_sidebar_border_color': u'C0DEED', 
+			u'id_str': u'358435647', 
+			u'profile_background_color': u'C0DEED', 
+			u'listed_count': 42, 
+			u'profile_background_image_url_https': u'https://abs.twimg.com/images/themes/theme1/bg.png', 
+			u'utc_offset': None, 
+			u'statuses_count': 42976, 
+			u'description': u"Twitter allows me to yell, hey you kids, get off my lawn. So the universe can hear. Follow me, I'll appreciate it. Not real fond of trump. #BeGoneTrump.", 
+			u'friends_count': 1657, 
+			u'location': None, 
+			u'profile_link_color': u'1DA1F2', 
+			u'profile_image_url': u'http://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+			u'following': None, 
+			u'geo_enabled': True, 
+			u'profile_background_image_url': u'http://abs.twimg.com/images/themes/theme1/bg.png', 
+			u'name': u'Paul T Murphy', 
+			u'lang': u'en', 
+			u'profile_background_tile': False, 
+			u'favourites_count': 10718, 
+			u'screen_name': u'Ptmurf1016', 
+			u'notifications': None, 
+			u'url': None, 
+			u'created_at': u'Fri Aug 19 22:45:37 +0000 2011', 
+			u'contributors_enabled': False, 
+			u'time_zone': None, 
+			u'protected': False, 
+			u'translator_type': u'none', 
+			u'is_translator': False
+		}, 
+		id=358435647,
+		 _api=<tweepy.api.API object at 0x06AD9050>, 
+		 verified=False, 
+		 translator_type=u'none', 
+		 profile_image_url_https=u'https://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+		 profile_sidebar_fill_color=u'DDEEF6', 
+		 is_translator=False, 
+		 geo_enabled=True, 
+		 profile_text_color=u'333333', 
+		 followers_count=1302, 
+		 protected=False, 
+		 location=None, 
+		 default_profile_image=False, 
+		 id_str=u'358435647', 
+		 utc_offset=None, 
+		 statuses_count=42976, 
+		 description=u"Twitter allows me to yell, hey you kids, get off my lawn. So the universe can hear. Follow me, I'll appreciate it. Not real fond of trump. #BeGoneTrump.", 
+		 friends_count=1657, 
+		 profile_link_color=u'1DA1F2', 
+		 profile_image_url=u'http://pbs.twimg.com/profile_images/2572735600/1968732v_normal', 
+		 notifications=None, 
+		 profile_background_image_url_https=u'https://abs.twimg.com/images/themes/theme1/bg.png', 
+		 profile_background_color=u'C0DEED', 
+		 profile_background_image_url=u'http://abs.twimg.com/images/themes/theme1/bg.png', 
+		 screen_name=u'Ptmurf1016', 
+		 lang=u'en', 
+		 profile_background_tile=False,
+		 favourites_count=10718, 
+		 name=u'Paul T Murphy', 
+		 url=None, 
+		 created_at=datetime.datetime(2011, 8, 19, 22, 45, 37), 
+		 contributors_enabled=False, 
+		 time_zone=None, 
+		 profile_sidebar_border_color=u'C0DEED', 
+		 default_profile=True, following=False, listed_count=42), 
+	filter_level=u'low', 
+	in_reply_to_status_id_str=u'940584383605563392', 
+	place=None, 
+	retweeted=False
+)
